041e733528ed909f5fac704747ff58520781ec75
Talha_Aydin <mt.aydin@outlook.com>
2022-01-14 19:57:38 +0300

    file structure set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Cards, Chart, CountryPicker } from './components/export';
+import styles from './App.module.css';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>App</h1>
+      <div className={styles.container}>
+        <Cards></Cards>
+        <Chart></Chart>
+        <CountryPicker></CountryPicker>
       </div>
     );
   }
